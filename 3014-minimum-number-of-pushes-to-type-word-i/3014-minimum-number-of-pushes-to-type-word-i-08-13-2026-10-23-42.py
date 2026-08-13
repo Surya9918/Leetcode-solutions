2ac8d5cc67ae47913ab5b756e9cebacb4f6e5d9d
Nallam Surya Saketharam <suryasaketharam@gmail.com>
2026-08-13 10:24:06 +0530

    08-13-2026 .py Math 0 ms (100%) 19.2 MB (85.11%)

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i-08-13-2026-10-23-42.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        n=len(word)
+        
+        if n <= 8:
+            return n
+         
+        elif n <= 16 :
+            return (n - 8) * 2 + 8
+        elif n <= 24 :
+            return (n - 16) * 3 + (8 * 2) + 8
+        else:
+            return (n - 24) * 4 + (8 * 3) + (8 * 2) + 8
+        
