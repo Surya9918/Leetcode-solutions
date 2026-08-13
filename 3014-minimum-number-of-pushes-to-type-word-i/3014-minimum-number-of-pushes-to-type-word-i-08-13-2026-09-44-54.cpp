b1a6ee9f5704f88862e4b46aee4a512632cb8ac0
Nallam Surya Saketharam <suryasaketharam@gmail.com>
2026-08-13 09:45:18 +0530

    08-13-2026 .cpp Math 0 ms (100%) 8.3 MB (74.61%)

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i-08-13-2026-09-44-54.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n = word.length();
+        
+        if (n <= 8) {
+            return n;
+        } 
+        else if (n <= 16) {
+            return (n - 8) * 2 + 8;
+        } 
+        else if (n <= 24) {
+            return (n - 16) * 3 + (8 * 2) + 8;
+        } 
+        else {
+            return (n - 24) * 4 + (8 * 3) + (8 * 2) + 8;
+        }
+    }
+};
